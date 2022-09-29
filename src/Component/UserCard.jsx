4e0f2f5e5bf1b8150d4b03f0f3e28c7b85f7ec8f
Yogesh Singh <singhyogesh2104@gmail.com>
2022-09-29 21:15:51 +0530

    update

--- src/Component/UserCard.jsx
+++ src/Component/UserCard.jsx
@@ -1,26 +1,28 @@
 import React,{Fragment} from 'react'
 
 const UserCard = (props) => {
-  console.log(props.datas)
+  // console.log(props.datas)
   return (
     <Fragment>
       <div className="flex justify-center">
         <div className="rounded-xl bg-white shadow-md">
           <div className="float-left mr-4 mt-10 ml-5 flex justify-center">
             <img
-              src={props.datas.picture.large}
+              // src={props.datas.picture.large}
               className="w-20 rounded-full"
               alt="Avatar"
             />
           </div>
           <div className="float-right m-6">
             <p className="text-4xl font-bold text-red-600 underline">
-            
+            {props.datas.name.title}{props.datas.name.first}{props.datas.name.last}
             </p>
             <p className="mr-5 mt-2 whitespace-pre-line text-sm">
             {props.datas.location.street.number} {props.datas.location.street.number}, Espirito Santo,<b>{props.datas.location.city},</b>{props.datas.location.postcode} {props.datas.location.timezone.offset}
             </p>
-              <span>{props.datas.location.timezone.description}</span>
+              <span>
+                {props.datas.location.timezone.description}
+                </span>
               <p className="text-gray-300">{props.datas.gender}</p>
           </div>
         </div>
